95c43f653841f01e8f543dd1738565c4b755a8f9
Maximiliano <maximilianogalindo7@gmail.com>
2024-02-11 17:52:44 -0600

    VI for ttt done

--- ValuePolicy_Iteration/Gato/train_ValueIteration.py
+++ ValuePolicy_Iteration/Gato/train_ValueIteration.py
@@ -10,6 +10,6 @@ policy = {}
 for x in agent.policy:
     policy[str(x)] = agent.policy[x]
 
-with open("Policies/valueIterationGato.json", 'w') as json_file:
+with open("ValuePolicy_Iteration/Policies/valueIterationGato.json", 'w') as json_file:
     json.dump(policy, json_file)
 
